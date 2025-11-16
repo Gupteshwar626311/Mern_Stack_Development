de36dbc620a30ce26a01ca50324f15c333e9bb5b
Gupteshwar626311 <gupteshwardln123@gmail.com>
2025-11-16 09:13:32 +0530

    All File Added

---
+++ React/IntroReact/react-project-one/src/Components/UserCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const UserCard = () => {
+  return (
+    <div className='user-container'>
+      <p id='user-name'>Gupteshwar Babber</p>
+      <img id='user-img' src="" alt="Love" />
+      <p id='user-desc'>Description of love Babber</p>
+    </div>
+  )
+}
+
+export default UserCard
